ee043ea4be7b8fb033c68b786a7251c6e59c1287
GradyBreen <820428@mystma.org>
2020-04-08 17:21:11 +0000

    added top 5 movies (fixed)

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("1. John Wick 1");
         System.out.println("2. John Wick 2");
-        System.out.println("3. John Wick 2");
+        System.out.println("3. John Wick 3");
         System.out.println("4. Hunger Games: Mockingjay Part 1");
         System.out.println("5. Hunger Games");
     }
