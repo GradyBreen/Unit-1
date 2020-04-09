61cf73f20cbd8761957f845d1d760630559a6949
GradyBreen <820428@mystma.org>
2020-04-09 17:59:34 +0000

    Varriable data type

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,33 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. John Wick 1");
-        System.out.println("2. John Wick 2");
-        System.out.println("3. John Wick 3");
-        System.out.println("4. Hunger Games: Mockingjay Part 1");
-        System.out.println("5. Hunger Games");
+        boolean livesInAlbertville = false;
+        char firstLetterOfName = 'G';
+        byte toesAndFingers = 20;
+        short popStMike = 16399;
+        int popUsa = 329000000;
+        long popEarth = 770000000;
+        float small = 1.23e10f;
+        double verySmall = 1.23456e300d;
+
+
+
+
+System.out.println("What is an even smaller measurement?");
+System.out.println(verySmall);
+System.out.println("What is a very small measurement?");
+System.out.println(small);
+System.out.println("What is earth's population?");
+System.out.println(popEarth);
+System.out.println("What is the USA's population?");
+System.out.println(popUsa);
+System.out.println("What is the population of St Michael?");
+System.out.println(popStMike);
+System.out.println("How many fingers and toes does a typical person have?");
+System.out.println(toesAndFingers);       
+System.out.println("what is the first letter of your name?");
+System.out.println(firstLetterOfName);
+System.out.println("Do I live in Albertville?");
+System.out.println(livesInAlbertville);
     }
 }
