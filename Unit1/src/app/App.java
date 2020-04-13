b598081898f1efca755aae421a5eb43da01c381b
GradyBreen <820428@mystma.org>
2020-04-13 16:16:03 +0000

    MadLibs

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,33 +2,20 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesInAlbertville = false;
-        char firstLetterOfName = 'G';
-        byte toesAndFingers = 20;
-        short popStMike = 16399;
-        int popUsa = 329000000;
-        long popEarth = 770000000;
-        float small = 1.23e10f;
-        double verySmall = 1.23456e300d;
+     String person1 = "Tim";
+     String person2 = "Jerry";
+     String verb1 = "Sprint";
+     String verb2 = "Pull";
+     String verb3 = "Turn";
+     String food = "Tacos";
+     String dayOfWeek = "Tuesday";
+     String place1 = "Niarobi, Kenya";
+     String place2 = "Frankfurt, Germany";
+     String adjective1 = "Tall";
+     String adjective2 = "Russian";
+
+     System.out.println(person1 + " is a programmer who likes to "+ verb1 + ". Every " + dayOfWeek + " they make a " + adjective1 + " "+ food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2 + ". Sometimes, they go to the " + adjective2 + " " + place2 + " with " + person2 + " so they can " + verb3 + " together.");
 
 
-
-
-System.out.println("What is an even smaller measurement?");
-System.out.println(verySmall);
-System.out.println("What is a very small measurement?");
-System.out.println(small);
-System.out.println("What is earth's population?");
-System.out.println(popEarth);
-System.out.println("What is the USA's population?");
-System.out.println(popUsa);
-System.out.println("What is the population of St Michael?");
-System.out.println(popStMike);
-System.out.println("How many fingers and toes does a typical person have?");
-System.out.println(toesAndFingers);       
-System.out.println("what is the first letter of your name?");
-System.out.println(firstLetterOfName);
-System.out.println("Do I live in Albertville?");
-System.out.println(livesInAlbertville);
-    }
+System.out.println();}
 }
