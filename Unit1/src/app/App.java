30cd40978f7de70fca57e9fd2b27bd04058f875e
GradyBreen <820428@mystma.org>
2020-04-14 17:21:11 +0000

    WhileLoops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,21 +1,38 @@
 package app;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import jdk.nashorn.api.tree.WhileLoopTree;
+
 public class App {
     public static void main(String[] args) throws Exception {
-     String person1 = "Tim";
-     String person2 = "Jerry";
-     String verb1 = "Sprint";
-     String verb2 = "Pull";
-     String verb3 = "Turn";
-     String food = "Tacos";
-     String dayOfWeek = "Tuesday";
-     String place1 = "Niarobi, Kenya";
-     String place2 = "Frankfurt, Germany";
-     String adjective1 = "Tall";
-     String adjective2 = "Russian";
+    int count = 0;
+    String color = "red";
+    boolean ben = false;
 
-     System.out.println(person1 + " is a programmer who likes to "+ verb1 + ". Every " + dayOfWeek + " they make a " + adjective1 + " "+ food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2 + ". Sometimes, they go to the " + adjective2 + " " + place2 + " with " + person2 + " so they can " + verb3 + " together.");
+    while (count < 10) {
+        System.out.println("The count is " + count);   
+        count = count + 1;  
+    }
+    System.out.println("Count finished");
 
+    while (color == "Red"){
+    System.out.println("the color is " + color);
+    color = "green";
+    }
+   
+    System.out.println("color finished");
+    
+  
+    while (ben = false){
+        System.out.println("is Benjamin Franklin running for president? " + ben);
+        ben = true;
+    }
 
-System.out.println();}
-}
+    System.out.println("question finished");
+    }
+
+}   
+   
+   
+   
