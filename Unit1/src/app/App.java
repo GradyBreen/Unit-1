771c05ce43d3c7b3485ddae6a6c38d985bec6af5
GradyBreen <820428@mystma.org>
2020-04-16 18:26:02 +0000

    For loop mastery

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,50 +1,38 @@
 package app;
 
-import java.lang.ProcessBuilder.Redirect;
 
-import jdk.nashorn.api.tree.WhileLoopTree;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-    int count = 0;
+public class App{
+    public static void main(String[] args) throws Exception 
     
-
-    while (count <= 10) {
-        System.out.println("The count is " + count);   
-        count = count + 1;  
-    }
-    System.out.println("Count finished");
-
-    for (int i = 0; i <= 10; i = i +1){
-    System.out.println("The count is " + i);
-    System.out.println("Lets keep counting");
-    }
-    for (int u = 10; u <= 20; u = u +1){
-        System.out.println("The count is " + u);
-        System.out.println("Lets keep counting");
-    }
-    for (int o = 10; o <= 100; o = o +1){
-        System.out.println("The count is " + o);
-        System.out.println("Lets keep counting");
-    }
-    for (int p = 1000; p <= 256000; p = p +1){
-        System.out.println("The count is " + p);
-        System.out.println("Lets keep counting");
-    }
-    for (int l = 11; l <= 12; l = l +1){
-        System.out.println("The count is " + l);
-        System.out.println("Lets Keep counting");
-    }
-    for (int q = 10; q >= 0; q = q -1){
-        System.out.println("The count is " + q);
-        System.out.println("Lets Keep counting");
+    
+{
+    for (int i = 22; i <= 28; i = i +3){
+        System.out.println(i);
     }
 
+    for (int i = 2; i <= 32; i = i *2){
+        System.out.println(i);
+    }
 
+    for (int i = 3; i >= -81; i = -i*i){
+        System.out.println(i);
+    }
 
-}   
+    for (int i = 50; i >= 5; i = i - 5){
+        System.out.println(i);
+    }
+
+    for (int i = 1; i <= 9; i = i + 1){
+        System.out.println(i * i);
+        System.out.println(i +3);
+        System.out.println(i);
+    }
+}
+
+}
+       
 
 
    
-}
+
    
