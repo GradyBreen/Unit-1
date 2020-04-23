1b1288efe3a95ceb7701b16daa7348190b999e4f
GradyBreen <820428@mystma.org>
2020-04-23 16:57:03 +0000

    Burgers Fries fast food assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,33 +3,27 @@ package app;
 public class App {
     public static void main(final String[] args) throws Exception{
 
-        System.out.println("Start first Loop");
+       System.out.println("Double or single digit?");
+      { for (int i = 20; i>0; i= i- 1)
+       if (i > 9) System.out.println(i + " is double digit");
+       else System.out.println(i + " is single digit");}
 
-        for (int i = 20; i >0; i--){
-        if (i == 10){
-        int x = 1;
-        while (x <= 6){
-        System.out.println(i);
-        x++;
-       } 
+
+        System.out.println("Nice!");
+        System.out.println("Fast Food");
+        {for (int i = 1; i < 6; i ++)
+        if (i == 100 ){}
        
-        }else{
-        System.out.println(i);
-     }  
-    }  
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
+          else {
+        System.out.println("Burgers");
+        System.out.println("Fires");}
+    System.out.println("Nice!");
+    }
 
-        for(int i = 0; 36 >= i; i = i + 4){
-        if(i % 6 == 0){
-        System.out.println(i);
-        System.out.println("Hey!");}
-        
-    else{
-      System.out.println(i);
-     }
-        }
- }
 }
+
+    }
+    
+    
         
 
