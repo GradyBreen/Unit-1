f3faa591f493e7bbd0552800c1081ae34cb3cce5
GradyBreen <820428@mystma.org>
2020-04-24 17:53:38 +0000

    Day 19 quiz 1

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,26 +3,28 @@ package app;
 public class App {
     public static void main(final String[] args) throws Exception{
 
-       System.out.println("Double or single digit?");
-      { for (int i = 20; i>0; i= i- 1)
-       if (i > 9) System.out.println(i + " is double digit");
-       else System.out.println(i + " is single digit");}
+      System.out.println("Start First Loop");
 
+      {for( int i = 1; i<11; i++)
+      if (i == 5){
+        System.out.println("five");
+      }else {
+        System.out.println(i);
+    }}
+    System.out.println("End first loop");
+    System.out.println("Start second loop");
 
-        System.out.println("Nice!");
-        System.out.println("Fast Food");
-        {for (int i = 1; i < 6; i ++)
-        if (i == 100 ){}
-       
-          else {
-        System.out.println("Burgers");
-        System.out.println("Fires");}
-    System.out.println("Nice!");
+    for (int x = 100; x < 201; x=x+10)
+    if (x == 150) { for (int j = 150; j < 161; j++)
+        System.out.println(j);
+    }else{
+        System.out.println(x);
     }
-
-}
-
+    System.out.println("End second loop");
     }
+   }
+    
+    
     
     
         
