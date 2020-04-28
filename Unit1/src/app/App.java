7195f47fe659743abfdafc17e06e3218f3f190cd
GradyBreen <820428@mystma.org>
2020-04-28 17:32:10 +0000

    First day of object; Scanner

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,24 @@
 package app;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(final String[] args) throws Exception{
 
-      System.out.println("Start First Loop");
+      Scanner input = new Scanner(System.in);
 
-      {for( int i = 1; i<11; i++)
-      if (i == 5){
-        System.out.println("five");
-      }else {
-        System.out.println(i);
-    }}
-    System.out.println("End first loop");
-    System.out.println("Start second loop");
+    System.out.println("Please enter your first name.");
+    String firstName = input.nextLine();
+    System.out.println("Please enter your last name.");
+    String lastName = input.nextLine();
+    System.out.println("Please enter your age");
+    String age = input.nextLine();
+    System.out.println("Please enter your grade number");
+    String grade = input.nextLine();
+    System.out.println("Please enter your favorite color");
+    String color = input.nextLine();
+    System.out.println("Hello, " + firstName + " " + lastName + " you are " + age + " years old, in grade " + grade + ", and you're favorite color is " + color );
 
-    for (int x = 100; x < 201; x=x+10)
-    if (x == 150) { for (int j = 150; j < 161; j++)
-        System.out.println(j);
-    }else{
-        System.out.println(x);
-    }
-    System.out.println("End second loop");
     }
    }
     
