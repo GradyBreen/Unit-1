79611727549899746708aabf3e268a72a5e00b36
GradyBreen <820428@mystma.org>
2020-05-05 17:22:41 +0000

    3 questions/Day 24

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,18 +7,33 @@ public class App {
 
       Scanner input = new Scanner(System.in);
     
-        System.out.println("What is the count?");
-        int count = input.nextInt();
+        System.out.println("How many inches are in a foot?");
+        String foot = input.nextLine();
+        if (foot.equals("12")){
+        System.out.println("Correct");}
+        else {System.out.println("Incorrect");}
 
 
-        for (int x = 1; x <= count; x++){
-       System.out.println(x);}
-
-
+        System.out.println("What is the capital of Minnesota?");
+        String capital = input.nextLine();
+        if (capital.equals("St Paul")){
+            System.out.println("Correct");
+        }else{System.out.println("Incorrect");}
+       
+        System.out.println("What year is it?");
+        String year = input.nextLine();
+        if (year.equals("2020")){
+            System.out.println("Correct");
+        }else{System.out.println("Incorrect");}
+    
+    
+    
+    }
+        
         
     }
 
-    }
+    
 
    
 
