8db39a46e6eba7a1bae503104cb85a86e12deabc
GradyBreen <820428@mystma.org>
2020-05-06 17:12:53 +0000

    Mad Lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,28 +7,31 @@ public class App {
 
       Scanner input = new Scanner(System.in);
     
-        System.out.println("How many inches are in a foot?");
-        String foot = input.nextLine();
-        if (foot.equals("12")){
-        System.out.println("Correct");}
-        else {System.out.println("Incorrect");}
-
-
-        System.out.println("What is the capital of Minnesota?");
-        String capital = input.nextLine();
-        if (capital.equals("St Paul")){
-            System.out.println("Correct");
-        }else{System.out.println("Incorrect");}
-       
-        System.out.println("What year is it?");
-        String year = input.nextLine();
-        if (year.equals("2020")){
-            System.out.println("Correct");
-        }else{System.out.println("Incorrect");}
-    
-    
-    
+        System.out.println("Enter a noun");
+        String noun = input.nextLine();
+        System.out.println("Enter a name");
+        String name = input.nextLine();
+        System.out.println("Enter a verb");
+        String verb = input.nextLine();
+        System.out.println("Enter an adjvective");
+        String adjective = input.nextLine();
+        System.out.println("Enter a place");
+        String place = input.nextLine();
+        System.out.println("Enter a food");
+        String food = input.nextLine();
+        System.out.println("Enter an animal");
+        String animal = input.nextLine();
+        System.out.println("Enter a number");
+        String number = input.nextLine();    
+   
+   
+        System.out.println("There was once a guy nicknamed " + noun + " " + name + ". He " + verb + " down the street to the " + adjective + " " + place + " to get his favorite food, a " + food + " famously made of " + animal + " he ordered " + number + " of this amazing food. He enjoyed it thoroughly");
+   
+   
+   
     }
+
+
         
         
     }
